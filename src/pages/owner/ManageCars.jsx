ca56cb2e6b961adaa5fa024aeda73ef8c69a025d
Chab Phanith <chabphanith2822@gmail.com>
2026-05-04 16:46:43 +0700

    updaded

--- src/pages/owner/ManageCars.jsx
+++ src/pages/owner/ManageCars.jsx
@@ -69,6 +69,7 @@ const ManageCars = () => {
 				<table className='w-full border-collapse text-left text-sm text-gray-600'>
 					<thead className='text-gray-500'>
 						<tr>
+							<th className="p-3 font-medium">No.</th>
 							<th className="p-3 font-medium">Car</th>
 							<th className="p-3 font-medium max-md:hidden">Category</th>
 							<th className="p-3 font-medium">Price</th>
@@ -80,6 +81,11 @@ const ManageCars = () => {
 					<tbody>
 						{cars.map((car, index)=>(
 							<tr key={index} className='border border-borderColor'>
+
+								<td className="px-4 py-3 text-gray-600 font-medium">
+									{index + 1}
+								</td>
+
 								<td className='p-3 flex items-center gap-3'>
 									<img src={car.image} alt="" className='h-12 w-12 aspect-square rounded-md object-cover'/>
 									<div className='max-md:hidden'>
